716c95149b35f6cbe6144d07e98fda0d4cfe881d
sheeddy <sheeddy_oh@hotmail.com>
2016-03-08 16:24:23 -0430

    sdf
    sdf

--- bd/data_base.sql
+++ bd/data_base.sql
@@ -11,7 +11,7 @@ USE inventario_maquinas;
 
 
 CREATE TABLE IF NOT EXISTS `departamentos` (
-id_departamento
+id_departamento int NOT NULL,
 departamento varchar (20) NOT NULL,
 PRIMARY KEY (id_departamento) NOT NULL AUTO_INCREMENT,
 
@@ -20,7 +20,7 @@ PRIMARY KEY (id_departamento) NOT NULL AUTO_INCREMENT,
 
 
 
-CREATE TABLE IF NOT EXISTS `dep_pers` (
+CREATE TABLE IF NOT EXISTS `dto_pers` (
 n_proceso int NOT NULL,
 fecha_de_inicio date NOT NULL,
 fecha_de_entrega date NOT NULL,
@@ -42,18 +42,13 @@ PRIMARY KEY () NOT NULL AUTO_INCREMENT,
 
 
 
-
-
-CREATE TABLE IF NOT EXISTS `tipo` (
- 
-cod_cpu varchar (30) NOT NULL, cpu varchar (30) NOT NULL,
-cod_monitor varchar (30) NOT NULL, monitor varchar (30) NOT NULL,
-cod_teclado varchar (30) NOT NULL, teclado varchar (30) NOT NULL,
-cod_mouse varchar (30) NOT NULL, mouse varchar (30) NOT NULL,
-cod_cornetas varchar (30) NOT NULL, cornetas varchar (30) NOT NULL,
-cod_impresora varchar (30) NOT NULL, impresora varchar (30) NOT NULL,
-cod_camara varchar (30) NOT NULL, camara varchar (30) NOT NULL,
-
+CREATE TABLE IF NOT EXISTS `esp_hard` (
+ cod_hard int NOT NULL,
+ serial_ varchar(30) NOT NULL,
+ marca varchar(30) NOT NULL,
+ estado varchar(30) NOT NULL,
+ f_ingreso date NOT NULL,
+ f_retiro date,
 
 PRIMARY KEY ()
 
@@ -61,8 +56,11 @@ PRIMARY KEY ()
 
 
 
-CREATE TABLE IF NOT EXISTS `quit_equipo` (
+
+CREATE TABLE IF NOT EXISTS `tipo_hardw` (
  
+cod_cpu int NOT NULL,
+tipo_nombre varchar(30) NOT NULL,
 
 PRIMARY KEY ()
 
@@ -70,26 +68,24 @@ PRIMARY KEY ()
 
 
 
-CREATE TABLE IF NOT EXISTS `auditoria_maquinas` (
- 
 
-sistema_operativo varchar (30) NOT NULL,
-punto_red varchar (30) NOT NULL,
-procesador  varchar (30) NOT NULL,
-tarjeta_madre varchar (30) NOT NULL,
-memoria_ram varchar (30) NOT NULL,
-tarjeta_red varchar (30),tarjeta_red2 varchar (30),
-tarjeta_wifi  varchar (30),
-Fuente_poder varchar (30) NOT NULL,
-cd_dvd varchar (30),
-disco_duro varchar (30) NOT NULL,disco_duro2 varchar (30),
-CASE_PC varchar (30) NOT NULL,
-tarjeta_video varchar (30),
+
+CREATE TABLE IF NOT EXISTS `especificacion_cpu` (
+ id_cpu int NOT NULL,
+ especificacion varchar(30) NOT NULL,
 
 
 
 
 )ENGINE=MyISAM DEFAULT CHARACTER utf8_unicode_ci;
 
+
+
+CREATE TABLE IF NOT EXISTS `tipo_hardw_cpu` (
+ id_h_cpu int NOT NULL,
+ tipo_nombre varchar(30) NOT NULL,
+
+
+)ENGINE=MyISAM DEFAULT CHARACTER utf8_unicode_ci;
 
 
